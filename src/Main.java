b94545fcceb5d8d8720e59cb2d6a8197d95a16cd
annaklimenko <annaklimenko5248@gmail.com>
2023-04-29 17:48:40 +0300

    Добавили Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a = 3;
-        int b = 3;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a = scn.nextInt();
+        System.out.println("Enter b:");
+        int b = scn.nextInt();
         System.out.println(a+b);
     }
 }
