bbba1e459be2e3de2eb0aed782d83699d60aab17
annaklimenko <annaklimenko5248@gmail.com>
2023-05-01 12:20:46 +0300

    Добавили вычитание

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Enter b:");
         int b = scn.nextInt();
         System.out.println(a+b);
+        System.out.println(a-b);
         System.out.println("Good bue");
     }
 }
